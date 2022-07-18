0e580f58f03a0383dafc96a771f872267573c122
tanvir-ux <tanviralamshuvo@gmail.com>
2022-07-18 12:51:49 +0600

    Facade Pattern Example

---
+++ design_patterns/facade/Iphone.java
@@ -0,0 +1,11 @@
+public class Iphone implements MobileShop {
+    @Override
+    public void modelNo() {
+        System.out.println("Iphone 13 pro max");
+    }
+
+    @Override
+    public void price() {
+        System.out.println("Price: 1,20,000 Taka");
+    }
+}
---
+++ design_patterns/facade/Samsung.java
@@ -0,0 +1,11 @@
+public class Samsung implements MobileShop {
+    @Override
+    public void modelNo() {
+        System.out.println("Samsung S8");
+    }
+
+    @Override
+    public void price() {
+        System.out.println("Price: 55,000 Taka");
+    }
+}
